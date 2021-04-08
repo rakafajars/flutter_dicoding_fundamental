59d8037df8399618c2d47f1365ac2c2e7bd76c58
Raka <raka@eudeka.id>
2021-04-08 08:06:27 +0700

    Gesture Detector

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,106 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-     home: MyHome(),
+      home: MyHome(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  final double boxSize = 150;
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+
+  /// untuk position
+  double posX = 0.0;
+  double posY = 0.0;
+
+  /// fungsi mendapatkan posisi ditengah
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30;
+
+    setState(() {
+      this.posX = posX;
+      this.posY = posY;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (posX == 0) {
+      center(context);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Halo'),
       ),
+      bottomNavigationBar: Container(
+        color: Colors.yellow,
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          'Taps: $numTaps - Double Taps: $numDoubleTaps - Long Press: $numLongPress',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: posY,
+            left: posX,
+            child: GestureDetector(
+              // onVerticalDragUpdate: (DragUpdateDetails details) {
+              //   print(details);
+              //   setState(() {
+              //     double delta = details.delta.dy;
+              //     posY += delta;
+              //   });
+              // },
+              // onHorizontalDragUpdate: (DragUpdateDetails details) {
+              //   setState(() {
+              //     double delta = details.delta.dx;
+              //     posX += delta;
+              //   });
+              // },
+              onPanUpdate: (DragUpdateDetails details) {
+                setState(() {
+                  double deltaX = details.delta.dx;
+                  double deltaY = details.delta.dy;
+
+                  posX += deltaX;
+                  posY += deltaY;
+                });
+              },
+              onTap: () {
+                setState(() {
+                  numTaps++;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  numDoubleTaps++;
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  numLongPress++;
+                });
+              },
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
