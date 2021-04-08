eed6c0efa47aa50e0009b8d9394eea042e46d8dc
Raka <raka@eudeka.id>
2021-04-08 08:57:21 +0700

    Calculator

---
+++ lib/calculator_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatelessWidget {
+  final Color backgroundColor;
+  final Color foregroundColor;
+  IconData icon;
+  final String text;
+  final Function onTap;
+
+  CalculatorButton({
+    @required this.backgroundColor,
+    @required this.foregroundColor,
+    @required this.text,
+    @required this.onTap,
+  });
+
+  CalculatorButton.Icon({
+    @required this.backgroundColor,
+    @required this.foregroundColor,
+    @required this.icon,
+    @required this.text,
+    @required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: backgroundColor,
+        child: Center(
+          child: icon == null
+              ? Text(
+                  text,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      .copyWith(color: foregroundColor),
+                )
+              : Icon(
+                  icon,
+                  color: foregroundColor,
+                ),
+        ),
+      ),
+    );
+  }
+}
