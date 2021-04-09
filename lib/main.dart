ea31b5e6c3d9dd1a29a4045505338073d6402caa
Raka <raka@eudeka.id>
2021-04-09 08:28:40 +0700

    Belajar Animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,20 +16,66 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-     home: MyHome(),
+      home: AnimationPage(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
+class AnimationPage extends StatefulWidget {
+  @override
+  _AnimationPageState createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage> {
+  bool _isBag = false;
+  double _size = 100.0;
+
+  Tween _animationTween = Tween<double>(begin: 0, end: pi * 2);
+  Tween _colorTween = ColorTween(begin: Colors.blue, end: Colors.purple);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Halo'),
+        title: Text('Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder<Color>(
+              // tween: _animationTween,
+              tween: _colorTween,
+              duration: Duration(seconds: 3),
+              builder: (context, Color value, child) {
+                return Container(
+                  color: value,
+                  height: _size,
+                  width: _size,
+                );                // return Transform.rotate(
+                //   angle: value,
+                //   child: AnimatedContainer(
+                //     duration: Duration(seconds: 1),
+                //     color: Colors.blue,
+                //     height: _size,
+                //     width: _size,
+                //   ),
+                // );
+              },
+            ),
+            RaisedButton(
+              child: Text('Animate'),
+              onPressed: () {
+                setState(() {
+                  _size = _isBag ? 200 : 100;
+                  _isBag = !_isBag;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
