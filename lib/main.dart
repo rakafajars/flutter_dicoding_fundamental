96480b276780ff0a946b9dfe3bbab47ae2de1980
Raka Fajar Salinggih <raka@eudeka.id>
2021-04-20 04:39:14 +0700

    Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,209 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dicoding_fundamental/custome_widget/custom_button.dart';
 import 'package:flutter_dicoding_fundamental/theme/style.dart';
 import 'package:flutter_dicoding_fundamental/ui/article_web_view.dart';
 import 'package:flutter_dicoding_fundamental/ui/detail_article.dart';
 import 'package:flutter_dicoding_fundamental/ui/home_page.dart';
 import 'package:flutter_dicoding_fundamental/ui/list_page.dart';
+import 'package:flutter_dicoding_fundamental/utils/notification_helper.dart';
+import 'package:flutter_dicoding_fundamental/utils/received_notification.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
-  runApp(MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+
+  await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+  _notificationHelper.requestIOSPermissions(flutterLocalNotificationsPlugin);
+
+  runApp(
+    MaterialApp(
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+        DetailPage.routeName: (context) => DetailPage(),
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
+  static const routeName = "/";
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final NotificationHelper _notificationHelper = NotificationHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// Pemanggilan Fungsi notifikasi pada initState
+    /// bertujuan untuk fungsi yang hanya berjalan hanya 1 kali saja
+    _notificationHelper.configureSelectNotificationSubject(
+      context,
+      DetailPage.routeName,
+    );
+    _notificationHelper.configureDidReceiveLocalNotificationSubject(
+      context,
+      DetailPage.routeName,
+    );
+  }
+
+  @override
+  void dispose() {
+    /// memanggil variable lalu menyimpan di dispose
+    /// digunakan untuk agar tidak terjadinya kebocoran
+    /// pada stream
+    selectNotificationSubject.close();
+    didReceiveLocalNotificationSubject.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryColor: primaryColor,
-        accentColor: secondaryColor,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: myTextTheme,
-        appBarTheme: AppBarTheme(
-          textTheme: myTextTheme.apply(
-            bodyColor: Colors.black,
-          ),
-          elevation: 0.0,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple Notification'),
         ),
-        buttonTheme: ButtonThemeData(
-          buttonColor: secondaryColor,
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(0),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                CustomButton(
+                  text: 'Show plain notification with payload',
+                  onPressed: () async {
+                    await _notificationHelper
+                        .showNotification(flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Show plain notification that has no body with payload',
+                  onPressed: () async {
+                    await _notificationHelper.showNotificationWithNoBody(
+                        flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Show schedule notification with payload',
+                  onPressed: () async {
+                    await _notificationHelper
+                        .scheduleNotification(flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Show grouped notifications [Android]',
+                  onPressed: () async {
+                    await _notificationHelper.showGroupedNotifications(
+                        flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text:
+                      'Show progress notification - updates every second [Android]',
+                  onPressed: () async {
+                    await _notificationHelper.showProgressNotification(
+                        flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Show big picture notification [Android]',
+                  onPressed: () async {
+                    await _notificationHelper.showBigPictureNotification(
+                        flutterLocalNotificationsPlugin);
+                  },
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Show notification with attachment [iOS]',
+                  onPressed: () async {
+                    await _notificationHelper.showNotificationWithAttachment(
+                        flutterLocalNotificationsPlugin);
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
-      initialRoute: HomePage.routeName,
-      routes: {
-        HomePage.routeName: (context) => HomePage(),
-        NewsListPage.routeName: (context) => NewsListPage(),
-        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
-              article: ModalRoute.of(context).settings.arguments,
-            ),
-        ArticleWebView.routeName: (context) => ArticleWebView(
-              article: ModalRoute.of(context).settings.arguments,
-            ),
-      },
     );
   }
 }
+
+class DetailPage extends StatelessWidget {
+  static const routeName = '/detail_page';
+
+  @override
+  Widget build(BuildContext context) {
+    final ReceivedNotification arg = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title : ${arg.payload}'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back!'),
+        ),
+      ),
+    );
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//         primaryColor: primaryColor,
+//         accentColor: secondaryColor,
+//         scaffoldBackgroundColor: Colors.white,
+//         textTheme: myTextTheme,
+//         appBarTheme: AppBarTheme(
+//           textTheme: myTextTheme.apply(
+//             bodyColor: Colors.black,
+//           ),
+//           elevation: 0.0,
+//         ),
+//         buttonTheme: ButtonThemeData(
+//           buttonColor: secondaryColor,
+//           textTheme: ButtonTextTheme.primary,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.all(
+//               Radius.circular(0),
+//             ),
+//           ),
+//         ),
+//       ),
+//       initialRoute: HomePage.routeName,
+//       routes: {
+//         HomePage.routeName: (context) => HomePage(),
+//         NewsListPage.routeName: (context) => NewsListPage(),
+//         ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+//               article: ModalRoute.of(context).settings.arguments,
+//             ),
+//         ArticleWebView.routeName: (context) => ArticleWebView(
+//               article: ModalRoute.of(context).settings.arguments,
+//             ),
+//       },
+//     );
+//   }
+// }
--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dicoding_fundamental/utils/received_notification.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:http/http.dart' as http;
 
 /// Membuat inisialisasi karna akan dipanggil pada top level dari aplikasi
 /// 2 variabel ini pun mengandung Stream yang dibuat agar aplikasi
@@ -111,40 +116,254 @@ class NotificationHelper {
       selectNotificationSubject.add(payload);
     });
   }
-}
 
-//
-// Future<void> showNotification(
-//     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
-//
-//   /// Mengatur konfigurasi notifikasi pada platform android
-//   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-//     _channelId,
-//     _channelName,
-//     _channelDesc,
-//     importance: Importance.max,
-//     priority: Priority.high,
-//     ticker: 'ticker',
-//   );
-//
-//   /// Mengatur konfigurasi notifikasi pada platform iOS
-//   var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-//
-//   /// Variable yang mengatur notifikasi untuk setiap platform yang telah dibuat
-//   /// sebelumnya
-//   var platformChannelSpecifics = NotificationDetails(
-//     android: androidPlatformChannelSpecifics,
-//     iOS: iOSPlatformChannelSpecifics,
-//   );
-//
-//   /// Fungsi yang akan menampilkan notifikasi disetiap platformnya
-//   /// property payload digunakan untuk meneruskan kembali
-//   /// kepada aplikasi, ketika notifikasi di ketuk.
-//   await flutterLocalNotificationsPlugin.show(
-//       0,
-//       'plain title',
-//       'plain body',
-//       platformChannelSpecifics,
-//       payload: 'plain notification'
-//   );
-// }
+  Future<void> showNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    /// Mengatur konfigurasi notifikasi pada platform android
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      _channelId,
+      _channelName,
+      _channelDesc,
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    /// Mengatur konfigurasi notifikasi pada platform iOS
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+    /// Variable yang mengatur notifikasi untuk setiap platform yang telah dibuat
+    /// sebelumnya
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    /// Fungsi yang akan menampilkan notifikasi disetiap platformnya
+    /// property payload digunakan untuk meneruskan kembali
+    /// kepada aplikasi, ketika notifikasi di ketuk.
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'plain notification');
+  }
+
+  Future<void> showNotificationWithNoBody(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        importance: Importance.max, priority: Priority.high, ticker: 'ticker');
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', null, platformChannelSpecifics,
+        payload: 'item x');
+  }
+
+  Future<void> scheduleNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var dateTime = DateTime.now().add(Duration(seconds: 5));
+    var vibrationPattern = Int64List(4);
+    vibrationPattern[0] = 0;
+    vibrationPattern[1] = 1000;
+    vibrationPattern[2] = 5000;
+    vibrationPattern[3] = 2000;
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      _channelId,
+      _channelName,
+      _channelDesc,
+      icon: 'secondary_icon',
+      sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+      largeIcon: DrawableResourceAndroidBitmap('sample_large_icon'),
+      vibrationPattern: vibrationPattern,
+      enableLights: true,
+      color: const Color.fromARGB(255, 255, 0, 0),
+      ledColor: const Color.fromARGB(255, 255, 0, 0),
+      ledOnMs: 1000,
+      ledOffMs: 500,
+    );
+
+    var iOSPlatformChannelSpecifics =
+        IOSNotificationDetails(sound: 'slow_spring_board.aiff');
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.schedule(
+      0,
+      'scheduled title',
+      'scheduled body',
+      dateTime,
+      platformChannelSpecifics,
+      payload: 'scheduled notification',
+    );
+  }
+
+  Future<void> showGroupedNotifications(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var groupKey = 'com.android.example.WORK_EMAIL';
+
+    var firstNotificationAndroidSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        importance: Importance.max,
+        priority: Priority.high,
+        groupKey: groupKey);
+    var firstNotificationPlatformSpecifics = NotificationDetails(
+      android: firstNotificationAndroidSpecifics,
+      iOS: null,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      1,
+      'Alex Faarborg',
+      'You will not believe...',
+      firstNotificationPlatformSpecifics,
+    );
+
+    var secondNotificationAndroidSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        importance: Importance.max,
+        priority: Priority.high,
+        groupKey: groupKey);
+    var secondNotificationPlatformSpecifics = NotificationDetails(
+      android: secondNotificationAndroidSpecifics,
+      iOS: null,
+    );
+    await flutterLocalNotificationsPlugin.show(
+        2,
+        'Jeff Chang',
+        'Please join us to celebrate the...',
+        secondNotificationPlatformSpecifics);
+
+    var lines = List<String>();
+    lines.add('Alex Faarborg  Check this out');
+    lines.add('Jeff Chang    Launch Party');
+
+    var inboxStyleInformation = InboxStyleInformation(lines,
+        contentTitle: '2 messages', summaryText: 'janedoe@example.com');
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        styleInformation: inboxStyleInformation,
+        groupKey: groupKey,
+        setAsGroupSummary: true);
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: null,
+    );
+    await flutterLocalNotificationsPlugin.show(
+        3, 'Attention', 'Two messages', platformChannelSpecifics);
+  }
+
+  Future<void> showProgressNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var maxProgress = 5;
+    for (var i = 0; i <= maxProgress; i++) {
+      await Future.delayed(Duration(seconds: 1), () async {
+        var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+            _channelId, _channelName, _channelDesc,
+            channelShowBadge: false,
+            importance: Importance.max,
+            priority: Priority.high,
+            onlyAlertOnce: true,
+            showProgress: true,
+            maxProgress: maxProgress,
+            progress: i);
+
+        var platformChannelSpecifics = NotificationDetails(
+          android: androidPlatformChannelSpecifics,
+          iOS: null,
+        );
+
+        await flutterLocalNotificationsPlugin.show(
+            0,
+            'progress notification title',
+            'progress notification body',
+            platformChannelSpecifics,
+            payload: 'item x');
+      });
+    }
+  }
+
+  /// Download File dan disimpan di path
+  Future<String> _downloadAndSaveFile(String url, String fileName) async {
+    var directory = await getApplicationDocumentsDirectory();
+    var filePath = '${directory.path}/$fileName';
+    var response = await http.get(url);
+    var file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
+
+  Future<void> showBigPictureNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var largeIconPath = await _downloadAndSaveFile(
+        'http://via.placeholder.com/48x48', 'largeIcon');
+    var bigPicturePath = await _downloadAndSaveFile(
+        'http://via.placeholder.com/400x800', 'bigPicture');
+
+    var bigPictureStyleInformation = BigPictureStyleInformation(
+        FilePathAndroidBitmap(bigPicturePath),
+        largeIcon: FilePathAndroidBitmap(largeIconPath),
+        contentTitle: 'overridden <b>big</b> content title',
+        htmlFormatContentTitle: true,
+        summaryText: 'summary <i>text</i>',
+        htmlFormatSummaryText: true);
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        styleInformation: bigPictureStyleInformation);
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: null,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+        0, 'big text title', 'silent body', platformChannelSpecifics);
+  }
+
+  Future<void> showNotificationWithAttachment(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var bigPicturePath = await _downloadAndSaveFile(
+        'http://via.placeholder.com/600x200', 'bigPicture.jpg');
+
+    var bigPictureAndroidStyle =
+        BigPictureStyleInformation(FilePathAndroidBitmap(bigPicturePath));
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDesc,
+        importance: Importance.high,
+        priority: Priority.high,
+        styleInformation: bigPictureAndroidStyle);
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails(
+        attachments: [IOSNotificationAttachment(bigPicturePath)]);
+
+    var notificationDetails = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+        0,
+        'notification with attachment title',
+        'notification with attachment body',
+        notificationDetails);
+  }
+
+  /// fungsi untuk ketika Notifikasi diketuk
+  void configureSelectNotificationSubject(BuildContext context, String route) {
+    selectNotificationSubject.stream.listen((String payload) async {
+      await Navigator.pushNamed(context, route,
+          arguments: ReceivedNotification(payload: payload));
+    });
+  }
+}
